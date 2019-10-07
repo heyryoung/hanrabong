b0c41157a7c2571f2c66783ad557d9d06250a0b7
unknown <leja0418@gmail.com>
2019-10-07 15:46:34 +0900

    first commit

---
+++ hanrabong/src/com/hanrabong/web/command/Receiver.java
@@ -0,0 +1,10 @@
+package com.hanrabong.web.command;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Receiver {
+	public static Command cmd = new Command();
+	public static void init(HttpServletRequest request) {
+		cmd = Commander.direct(request);
+	}
+}
